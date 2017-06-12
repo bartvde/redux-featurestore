bc8a9dbb864c4abb39220801e60c749c628e7b2b
bartvde <bartvde@boundlessgeo.com>
2017-06-12 17:36:39 +0200

    Show real data

--- main.js
+++ main.js
@@ -3,21 +3,28 @@ import {Provider, connect} from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var initialFeatures = {
-  type : 'FeatureCollection',
-  features : [
-    { "type": "Feature",
-      "geometry": {"type": "Point", "coordinates": [0, 1]},
-      "properties": {"prop0": "value0"}
-    }
-  ]
-};
-
-const features = (state = initialFeatures, action) => {
+const features = (state = [], action) => {
   switch (action.type) {
+    case 'ADD_FEATURES':
+      return state.concat(action.features);
+    break;
     default:
       return state;
   } 
+};
+
+const featureLoader = (url, store) => {
+  fetch(url)
+    .then(function(response) {
+    return response.json()
+  }).then(function(json) {
+    store.dispatch({
+      type: 'ADD_FEATURES',
+      features: json.features
+    });
+  }).catch(function(ex) {
+    console.log('parsing failed', ex)
+  });
 };
 
 const geojsonApp = combineReducers({
@@ -30,8 +37,8 @@ class FeatureTable extends React.Component {
   }
   render() {
     const {features} = this.props;
-    const featureItems = features.features.map(function(feature, idx) {
-      return (<li key={idx}>{feature.properties.prop0}</li>);
+    const featureItems = features.map(function(feature, idx) {
+      return (<li key={idx}>{feature.properties.cat}</li>);
     });
     return (<ul>{featureItems}</ul>);
   }
@@ -43,8 +50,11 @@ const mapStateToProps = (state) => {
 
 const FeaturesApp = connect(mapStateToProps)(FeatureTable);
 
+const store = createStore(geojsonApp);
+featureLoader('./airports.json', store);
+
 ReactDOM.render(
-  <Provider store={createStore(geojsonApp)}>
+  <Provider store={store}>
     <FeaturesApp/ >
   </Provider>
 , document.getElementById('map'));
