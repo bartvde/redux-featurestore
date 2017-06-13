6724b158423bad53c858d5cabb0fda8ef89b536d
bartvde <bartvde@boundlessgeo.com>
2017-06-13 10:03:52 +0200

    Add features in map

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ import {createStore, combineReducers} from 'redux';
 import {Provider, connect} from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Map from 'ol/map';
+import View from 'ol/view';
+import TileLayer from 'ol/layer/tile';
+import XYZ from 'ol/source/xyz';
+import VectorLayer from 'ol/layer/vector';
+import VectorSource from 'ol/source/vector';
+import GeoJSONFormat from 'ol/format/geojson';
 
 const features = (state = [], action) => {
   switch (action.type) {
@@ -40,7 +47,7 @@ const FeatureTable = ( {features} ) => {
     }
     rows.push(<tr key={idx}>{cells}</tr>);
   });
-  return (<table><tbody>{rows}</tbody></table>);
+  return (<div style={{position: 'absolute', left: 0, top: 0, width: '50%', height: '50%'}}><table><tbody>{rows}</tbody></table></div>);
 }
 
 const mapStateToProps = (state) => {
@@ -49,11 +56,50 @@ const mapStateToProps = (state) => {
 
 const FeaturesApp = connect(mapStateToProps)(FeatureTable);
 
+class VectorContainer extends React.Component {
+  componentDidMount() {
+    this._layer = new VectorLayer({
+      source: new VectorSource()
+    })
+    const map = new Map({
+      target: ReactDOM.findDOMNode(this.refs.map),
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          })
+        }),
+        this._layer
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
+    });
+  }
+  componentWillReceiveProps(nextProps) {
+    let features = [];
+    const format = new GeoJSONFormat();
+    for (var i = 0, ii = nextProps.features.length; i < ii; ++i) {
+      features.push(format.readFeature(nextProps.features[i]));
+    }
+    this._layer.getSource().addFeatures(features);
+  }
+  render() {
+    return (<div style={{position: 'absolute', right: 0, top: 0, width: '50%', height: '100%'}} ref='map'></div>);
+  }
+}
+
+VectorContainer = connect(mapStateToProps)(VectorContainer);
+
 const store = createStore(geojsonApp);
 featureLoader('./airports.json', store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <FeaturesApp/ >
+    <div>
+      <FeaturesApp/>
+      <VectorContainer/>
+    </div>
   </Provider>
 , document.getElementById('map'));
